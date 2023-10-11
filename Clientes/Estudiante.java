92dd81717606f2be4a5644fcd1234d0517b7e14b
gomiten1 <masmibe@gmail.com>
2023-10-10 18:28:16 -0600

    Cosas

--- Clientes/Estudiante.java
+++ Clientes/Estudiante.java
@@ -1,7 +1,12 @@
 package Clientes;
 
+import Productos.Laptop;
+import Productos.Celular;
+
 public class Estudiante extends Comprador{
     public Estudiante(int dinero, String nombre){
         super(dinero, nombre);
     } 
+
+
 }
--- Clientes/Socio.java
+++ Clientes/Socio.java
@@ -1,11 +1,17 @@
 package Clientes;
 
+import Productos.Television;
+import Productos.Tablet;
+
+
 public class Socio extends Comprador{
     public Socio(int dinero, String nombre){
         super(dinero, nombre);
-    } 
-
+    }
     
+    
+
+
 
     
 }
