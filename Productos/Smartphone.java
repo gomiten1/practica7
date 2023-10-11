77af19a8d4c02f1ea90b7e74afd2f18cf5e36afa
gomiten1 <masmibe@gmail.com>
2023-10-10 20:37:35 -0600

    cambios

--- Productos/Smartphone.java
+++ Productos/Smartphone.java
@@ -6,8 +6,8 @@ public class Smartphone extends Celular{
     String color;
     int cantidadMemoria;
 
-    public Smartphone(int codigo, double precio, String marca, String tipo, String sistema, int senial, int numero, String camara, String color, int cantidadMemoria){
-        super( codigo,  precio,  marca,  tipo,  sistema,  senial,  numero);
+    public Smartphone(int codigo, double precio, String marca, String sistema, int senial, int numero, String camara, String color, int cantidadMemoria){
+        super( codigo,  precio,  marca,  "Smartphone",  sistema,  senial,  numero);
 
         this.camara = camara;
         this.color = color;
--- Productos/Television.java
+++ Productos/Television.java
@@ -1,5 +1,21 @@
 package Productos;
 
 public class Television extends DispositivoElectronico {
-    
+    String tipo;
+
+    String modelo;
+    String senial;
+
+    public Television(int codigo, double precio, String marca, String tipo, String modelo, String senial){
+        super(codigo, precio, marca);
+
+        this.tipo = tipo;
+        this.modelo = modelo;
+        this.senial = senial;
+
+    }
+
+    public String toString(){
+        return super.toString() + ", tipo: " + tipo + ", modelo" + modelo + ", señal: " + senial;
+    }
 }
